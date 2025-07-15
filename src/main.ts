362e8cdedeaaa0f3c3258b3d791aa440b003e45a
ales <hi@alesnovotny.cz>
2025-07-15 19:23:34 +0200

    tuzing

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './new.css'
 import { WeekService } from './services/week_service';
 import { AppController } from './app_controller';
 
-const API: string = "https://todoz.app/api/0/";import.meta.env.API_BASE_PATH;
+const API: string = "https://www.144b.net/~perry/todoz/api";import.meta.env.API_BASE_PATH;
 
 const app = new AppController(new WeekService(API), document.body, document.querySelector('template')!);
 
