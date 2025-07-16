22011a09836d6df2831e320c4135e51596ab93a5
ales <hi@alesnovotny.cz>
2025-07-16 14:53:39 +0200

    wth

--- src/services/task_service.ts
+++ src/services/task_service.ts
@@ -1,7 +1,7 @@
 import { Task } from '../models/task';
 
 export class TaskService {
-    dueIn(task: Task): number {
+    dueIn = (task: Task): number => {
         if (!task.dueDate) {
             return 0;
         }
@@ -11,13 +11,13 @@ export class TaskService {
         const diff = dueDate.getTime() - now.getTime();
 
         return Math.ceil(diff / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
-    }
+    };
 
-    prioritySort(a: Task, b: Task): number {
+    prioritySort = (a: Task, b: Task): number => {
         return (b.priority - a.priority) * 1e6 + (this.dueIn(a) - this.dueIn(b)) * 1e5 + (a.id - b.id);
-    }
+    };
 
-    dueSort(a: Task, b: Task): number {
+    dueSort = (a: Task, b: Task): number => {
         return (this.dueIn(a) - this.dueIn(b));
-    }
+    };
 }
