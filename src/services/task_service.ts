d8c04115d28d9f04ac6bf5fea505385d2f14aaa5
ales <hi@alesnovotny.cz>
2025-07-29 21:10:41 +0200

    fox prioritySort

--- src/services/task_service.ts
+++ src/services/task_service.ts
@@ -3,18 +3,18 @@ import { Task } from '../models/task';
 export class TaskService {
     dueIn = (task: Task): number => {
         if (!task.dueDate) {
-            return 0;
+            return 0.5; // To be sorted after today's tasks but before tomorrow's
         }
 
         const now = new Date();
         const dueDate = new Date(task.dueDate);
         const diff = dueDate.getTime() - now.getTime();
 
-        return Math.ceil(diff / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+        return Math.floor(diff / (1000 * 60 * 60 * 24));
     };
 
     prioritySort = (a: Task, b: Task): number => {
-        return (b.priority - a.priority) * 1e5 + (this.dueIn(a) - this.dueIn(b)) * 1e6 + (a.id - b.id);
+        return (b.priority - a.priority) * 1e5 + (this.dueIn(a) - this.dueIn(b)) * 1e7 + (a.id - b.id);
     };
 
     dueSort = (a: Task, b: Task): number => {
